504a28ead9575c695e95d82b5a0a3f83d581b46b
João Lee <joaumlimaum@gmail.com>
2022-03-21 22:42:21 -0300

    Requisitos 4 e 5

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//2 - Adicione à página uma paleta contendo quatro cores distintas.
 let myBody = document.querySelector("body");
 let mainTag = document.createElement("main");
 let paletteSection = document.createElement("section");
@@ -7,16 +6,29 @@ mainTag.className = "main-content"
 mainTag.appendChild(paletteSection);
 paletteSection.id = "color-palette"
 
-
+//2 - Adicione à página uma paleta contendo quatro cores distintas.
+//3 - Adicione a cor preta como a primeira cor da paleta de cores.
 function divMaker() {
     for (let index = 0; index < 4; index += 1) {
-       let palette = document.createElement("div")
-       palette.className = "color"
-       palette.id = `colors${index}`
+       let palette = document.createElement("div");
+       palette.className = "color";
+       palette.id = `colors${index}`;
        paletteSection.appendChild(palette);
     }
 }
 divMaker()
+//4 - Adicione à página um quadro de pixels, com 25 pixels.
+let pixelSection = document.createElement("section")
+paletteSection.appendChild(pixelSection);
+pixelSection.id = "pixel-board";
+function pixelBoard(){
+    for (let index2 = 0; index2 <= 24; index2 += 1) {
+            let blocks = document.createElement("div");
+            blocks.className = "pixel";
+            pixelSection.appendChild(blocks);
+        }
+    }
+pixelBoard()
 
 
 // let paletteDivs = document.getElementsByClassName("color")[0];
