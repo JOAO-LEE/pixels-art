22fc4afac2e0fdcf19609908cc6f8150bc08c33d
João Lee <joaumlimaum@gmail.com>
2022-03-21 23:48:08 -0300

    Desafio 6

--- script.js
+++ script.js
@@ -11,13 +11,19 @@ paletteSection.id = "color-palette"
 function divMaker() {
     for (let index = 0; index < 4; index += 1) {
        let palette = document.createElement("div");
-       palette.className = "color";
+       palette.classList = "color";
        palette.id = `colors${index}`;
+       if (index === 0) {
+        palette.style.backgroundColor = "black";
+        palette.classList.add("selected");
+    }
        paletteSection.appendChild(palette);
     }
 }
 divMaker()
 //4 - Adicione à página um quadro de pixels, com 25 pixels.
+//5 - Faça com que cada elemento do quadro de pixels possua 40 pixels de largura, 40 pixels de altura e seja delimitado por uma borda preta de 1 pixel.
+//6 - Defina a cor preta como cor inicial. Ao carregar a página, a cor preta já deve estar selecionada para pintar os pixels
 let pixelSection = document.createElement("section")
 paletteSection.appendChild(pixelSection);
 pixelSection.id = "pixel-board";
@@ -29,8 +35,6 @@ function pixelBoard(){
         }
     }
 pixelBoard()
-
-
 // let paletteDivs = document.getElementsByClassName("color")[0];
 // function paletteColors () {
 //     for (let index = 0; index < myColors.length; index += 1){
