95eea24c3313cb2e15b3f7ec704738e78d76589a
João Lee <joaumlimaum@gmail.com>
2022-03-29 19:11:18 -0300

    Requisitos 8 & 9, correcoes de alguns nomes de variaveis

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ mainTag.className = 'main-content';
 mainTag.appendChild(paletteSection);
 paletteSection.id = 'color-palette';
 mainTag.appendChild(clearButton);
-
 //2 - Adicione à página uma paleta contendo quatro cores distintas.
 //3 - Adicione a cor preta como a primeira cor da paleta de cores.
 //6 - Defina a cor preta como cor inicial. Ao carregar a página, a cor preta já deve estar selecionada para pintar os pixels
@@ -15,13 +14,10 @@ function divMaker() {
     for (let index = 0; index < 4; index += 1) {
        let palette = document.createElement('div');
        palette.classList.add('color');
-    //    palette.id = `colors${index}`;
-    //    if (index === 0) {
-    //     palette.style.backgroundColor = 'black';
-    //     palette.classList.add('selected');
-    // }
        paletteSection.appendChild(palette);
     }
+}
+divMaker()
     let firstColor = document.querySelectorAll('div')[0];
     firstColor.style.backgroundColor = 'black';
     firstColor.classList.add('selected');
@@ -31,21 +27,20 @@ function divMaker() {
     thirdColor.style.backgroundColor = '#fe6d73';
     let fourthColor = document.querySelectorAll('div')[3];
     fourthColor.style.backgroundColor = '#17c3b2';
+// 8 - Clicar em um pixel dentro do quadro após selecionar uma cor na paleta faz com que o pixel seja preenchido com a cor selecionada.
+function pickColor(event){
+    // firstColor =     event.target.attributes.style.value.toString().split(':')[1].slice(0, -1)
+    let selectionColor = document.querySelector('.selected')
+    let selectedBg = selectionColor.style.backgroundColor
+    let dart = event.target
+    dart.style.backgroundColor = selectedBg
 }
-divMaker()
-// 9 - Crie um botão que, ao ser clicado, limpa o quadro preenchendo a cor de todos seus pixels com branco.
-// clearButton.innerText = 'Limpar';
-// clearButton.id = 'clear-board';
-// function clearAll(){
-//     let blocksToClear = document.querySelectorAll('section')[1].children;
-//     for (let xxx of blocksToClear){
-//         xxx.style.backgroundColor = 'white';
-//     }
-// }
-// clearButton.addEventListener('click', clearAll);
+function colorApplication(action2) {
+    let appliance = document.getElementById(action2)
+    appliance.style.backgroundColor = firstColor
+}
 //4 - Adicione à página um quadro de pixels, com 25 pixels.
 //5 - Faça com que cada elemento do quadro de pixels possua 40 pixels de largura, 40 pixels de altura e seja delimitado por uma borda preta de 1 pixel.
-
 let pixelSection = document.createElement('section');
 mainTag.appendChild(pixelSection);
 pixelSection.id = 'pixel-board';
@@ -53,18 +48,32 @@ function pixelBoard() {
     for (let index2 = 0; index2 <= 24; index2 += 1) {
             let blocks = document.createElement('div');
             blocks.className = 'pixel';
+            blocks.id = 'painting' + index2
             pixelSection.appendChild(blocks);
-        }
+            blocks.addEventListener('click', pickColor)
+        }   
     }
 pixelBoard()
 //7 - Clicar em uma das cores da paleta faz com que ela seja selecionada e utilizada para preencher os pixels no quadro.
 let colorPicker = document.querySelector('section').children
-for (let xyx of colorPicker){
-    xyx.addEventListener('click', colorSelection)
+for (let index3 of colorPicker) {
+    index3.addEventListener('click', colorSelection)
 }
-function colorSelection(colorizer){
-    for (let i of colorPicker){
-        i.classList.remove('selected')
+function colorSelection(colorizer) {
+    for (let index4 of colorPicker) {
+        index4.classList.remove('selected')
     }
     colorizer.target.classList.add('selected')                           
 }
+let paitingBlocks = document.querySelectorAll('.pixels')
+// 9 - Crie um botão que, ao ser clicado, limpa o quadro preenchendo a cor de todos seus pixels com branco.
+clearButton.innerText = 'Limpar';
+clearButton.id = 'clear-board';
+let buttonToClear = document.getElementById('clear-board')
+function clearAll() {
+    let blocksToClear = document.querySelectorAll('.pixel');
+    for (let xxx of blocksToClear) {
+        xxx.style.backgroundColor = 'white'
+    }
+}
+buttonToClear.addEventListener('click', clearAll);
